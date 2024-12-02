046e09983c58cff85dce33853a719238b6f8643d
Ralian <wbowers314@gmail.com>
2024-12-02 16:05:55 -0700

    Minor edits to scoped strings article

--- articles/2024.11.29/parser.py
+++ articles/2024.11.29/parser.py
@@ -1,12 +1,12 @@
 from lark import Lark, Transformer, Token
 
-class TreeToJson(Transformer):
+class Tree(Transformer):
     def string(self, s:str):
         return s[-1].lstrip("str").lstrip(" ").lstrip("{").rstrip("}")
     def number(self, n):
         return float(n[-1].lstrip("num").lstrip(" ").lstrip("{").rstrip("}"))
 
-json_parser = Lark(r"""
+parser = Lark(r"""
 WSP: /[ \t\n\f\r]/
 
 value: list
@@ -19,12 +19,8 @@ STRING: "str" WSP* "{" /(\s|\S)*?(?<!\\)(\\\\)*?/ "}"
 NUMBER: "num" WSP* "{" WSP* /-?\d+(\.\d+)?([eE][+-]?\d+)?/ WSP* "}"
 """, start='value')
 
-# bval: "true" -> true
-#     | "false" -> false
-# boolean: "bool" WSP* "{" WSP* bval WSP* "}"
-
 text = r"""list {str{item0}, str {ite
 m1}, num { 3.14 },
 num {3e8}}"""
 
-print(TreeToJson().transform(json_parser.parse(text)))
+print(Tree().transform(parser.parse(text)))
